2cbb79c43d17d718d2efbe37a600c01ccaf6e3a6
benma <mbencun@gmail.com>
2015-03-12 14:29:13 +0100

    Moved workaround for a permission bug to run_services.sh.
    While the RUN did execute before, the permission bug remains, probably
    because new fs layers are created on top.

--- golang-base/run_services.sh
+++ golang-base/run_services.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
+
+### Workaround for an AUFS bug: https://github.com/monetas/notary/issues/234
+mkdir /etc/ssl/private-copy
+mv /etc/ssl/private/* /etc/ssl/private-copy/
+rm -rf /etc/ssl/private
+mv /etc/ssl/private-copy /etc/ssl/private
+chown -R root:ssl-cert /etc/ssl/private/
+chmod -R 0750 /etc/ssl/private; 
+
+### Start services.
 service postgresql start
