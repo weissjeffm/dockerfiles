80525318287f1b734d839fc00cd4dbb7b4f721af
Guilherme Salgado <gsalgado@gmail.com>
2015-05-07 14:23:55 -0300

    Rename the test DB to notary_test_template and make notary_test_user its owner

--- golang-base/postgres/postgres_superuser.sql
+++ golang-base/postgres/postgres_superuser.sql
@@ -1,6 +1,4 @@
 -- Setup that needs to be run by a superuser.
--- See postgres_notary.sql for the setup of the schema etc.
-CREATE DATABASE notary_test;
 CREATE USER notary_test_user PASSWORD 'notary_test_pwd';
-GRANT ALL PRIVILEGES ON DATABASE notary_test TO notary_test_user;
-
+CREATE DATABASE notary_test_template;
+ALTER DATABASE notary_test_template OWNER TO notary_test_user;
